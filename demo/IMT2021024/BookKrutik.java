633e18f5499a1266f10ab658e1838c3dcc763df4
Krutik-Patel <krutikmpatel@gmail.com>
2023-01-06 20:48:06 +0530

    Final draft, added comments

--- demo/IMT2021024/BookKrutik.java
+++ demo/IMT2021024/BookKrutik.java
@@ -3,12 +3,15 @@ package demo.IMT2021024;
 import ecomm.Globals;
 
 public class BookKrutik extends ProductKrutik {
-    private Globals.Category category;
-    private String name;
-    private String productID;
-    private float price;
-    private int quantity;
 
+    // private data members
+    private Globals.Category category; // Mobile or Book: Category, set to book when initalized
+    private String name; // name of the book
+    private String productID; // productID of the book
+    private float price; // price of the book
+    private int quantity; // quantity of the book available with the seller
+
+    // constructor
     BookKrutik(String name, String productID, float price, int quantity) {
         this.category = Globals.Category.Book;
         this.name = name;
@@ -17,30 +20,38 @@ public class BookKrutik extends ProductKrutik {
         this.quantity = quantity;
     }
 
+    // getter and setter methods
+    @Override
     public Globals.Category getCategory() {
         return this.category;
     }
 
+    @Override
     public String getName() {
         return this.name;
     }
 
+    @Override
     public String getProductID() {
         return this.productID;
     }
 
+    @Override
     public float getPrice() {
         return this.price;
     }
 
+    @Override
     public int getQuantity() {
         return this.quantity;
     }
 
+    @Override
     protected void setPrice(float price) {
         this.price = price;
     }
 
+    @Override
     protected void setQuantity(int quantity) {
         this.quantity = quantity;
     }
