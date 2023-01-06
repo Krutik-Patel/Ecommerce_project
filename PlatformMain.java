0209c35fbc5de6a28346b4e821736af0bd58fc46
Suyashchavan10 <98649529+Suyashchavan10@users.noreply.github.com>
2023-01-06 12:05:07 +0530

    Update PlatformMain.java

--- PlatformMain.java
+++ PlatformMain.java
@@ -12,34 +12,19 @@ public class PlatformMain {
 
 		Platform pf = new DemoPlatform(); // replace with appropriate derived class
 
-		// create a number of sellers (of different sub-types of Seller)
-		// Assign a name (sellerID) to each of them.
+		// creating a number of sellers (of different sub-types of Seller)
+		// Assigning a name (sellerID) to each of them.
 
-		// replace each of the XYZ below with the derived class name of one of the
-		// team members.
-
-		// add sellers here
+		// adding sellers here
 		Seller s1 = new SellerKrutik("kurtik");
 		Seller s2 = new SellerSuyash("suyas");
 		Seller s3 = new SellerAnshuman("anshuman");
 		s1.addPlatform(pf);
 		s2.addPlatform(pf);
 		s3.addPlatform(pf);
-		/*
-		 * Seller s2 = new ada("Seller2");
-		 * s1.addPlatform(pf);
-		 * 
-		 * Seller s3 = new SellerXYZ2("Seller3");
-		 * s1.addPlatform(pf);
-		 */
-
-		/*
-		 * // keep reading from System.in
-		 * // If "Check" typed in
-		 * // invoke
-		 * pf.processRequests();
-		 */
+		
 		Scanner sc = new Scanner(System.in);
+		//checking that "check" command is there or not
 		while (1 > 0) {
 			String inp = sc.next();
 			if (inp.equals("Check"))
